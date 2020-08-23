f40925b919ade8bd712361aeec885d01c9a8096d
phatpham9 <phatpham9@gmail.com>
2020-08-24 00:43:05 +0700

    feat(bookings): add confirm & reject apis

--- src/components/bookings.ts
+++ src/components/bookings.ts
@@ -45,10 +45,23 @@ async function update({ id, ...data }: Booking): Promise<Booking> {
   });
 }
 
+async function confirm(id: string, date: Date): Promise<Booking> {
+  return await request.patch({
+    url: `${getBaseUrl()}/${id}/confirmation`,
+    body: { confirmedDate: date },
+  });
+}
+
+async function reject(id: string): Promise<Booking> {
+  return await request.patch({
+    url: `${getBaseUrl()}/${id}/rejection`,
+  });
+}
+
 async function del(id: string): Promise<Booking> {
   return await request.del({
     url: `${getBaseUrl()}/${id}`,
   });
 }
 
-export { list, count, create, get, update, del };
+export { list, count, create, get, update, confirm, reject, del };
